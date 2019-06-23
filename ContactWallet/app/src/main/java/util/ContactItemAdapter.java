8897f09ed750e2cf81a6b215c05a096d5cbdfdda
Peter Wang <xy38wang@uwaterloo.ca>
2019-06-23 16:25:32 -0400

    remove duplicate contactItemAdapter

--- ContactWallet/app/src/main/java/util/ContactItemAdapter.java
+++ ContactWallet/app/src/main/java/util/ContactItemAdapter.java
@@ -1,62 +1,52 @@
 package util;
 
 import android.content.Context;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.badgercubed.ContactWallet.R;
 
 import java.util.List;
 
-public class ContactItemAdapter extends BaseAdapter {
+public class ContactItemAdapter2 extends RecyclerView.Adapter<ContactItemAdapter2.ViewHolder> {
 
-    private final Context context;
-    private final List<model.ContactItem> contactItems;
+    public List<model.ContactItem> contactItems;
 
-    public ContactItemAdapter(Context context, List<model.ContactItem> contactItems) {
-        this.context = context;
+    public ContactItemAdapter2(List<model.ContactItem> contactItems) {
         this.contactItems = contactItems;
     }
 
     @Override
-    public int getCount() {
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+       viewHolder.textView.setText(contactItems.get(i).getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
         return contactItems.size();
     }
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
-    @Override
-    public Object getItem(int position) {
-        return contactItems.get(position);
-    }
+        View m_view;
+        public TextView textView;
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+        public ViewHolder(View itemView) {
+            super(itemView);
+            m_view = itemView;
 
-        final model.ContactItem contactItem = contactItems.get(position);
-
-        if (convertView == null) {
-            final LayoutInflater layoutInflater = LayoutInflater.from(context);
-            convertView = layoutInflater.inflate(R.layout.contact_items, null);
+            textView = m_view.findViewById(R.id.list_item_textview);
         }
-
-       // final ImageView imageView = (ImageView)convertView.findViewById(R.id.imageview_cover_art);
-        final TextView descTextView = (TextView)convertView.findViewById(R.id.textview_book_name);
-        final TextView protectionLevelTextView = (TextView)convertView.findViewById(R.id.textview_book_author);
-
-        // Get Service Image Here
-        // imageView.setImageResource(contactItem);
-        descTextView.setText(contactItem.getDescription());
-        protectionLevelTextView.setText(contactItem.getProtectionLevel().toString());
-
-        return convertView;
     }
 }
