87c19898a48f0d116772d8ad218fc9c62c60c7ed
David <david@loukidelis.com>
2019-07-17 14:35:46 -0400

    Cosmetic Updates (#13)
    * Update colours
    
    * Update to Material theme. Adds text on FABs.
    
    * Bug fix

--- ContactWallet/app/src/main/java/com/badgercubed/ContactWallet/dialog/AddContactDialog.java
+++ ContactWallet/app/src/main/java/com/badgercubed/ContactWallet/dialog/AddContactDialog.java
@@ -3,7 +3,7 @@ package com.badgercubed.ContactWallet.dialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
